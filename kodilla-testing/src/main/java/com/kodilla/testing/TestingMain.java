ea656bd1c4d73cc77f5a9a9574fa75544f5866f7
elorek <e.lorek@onet.pl>
2019-10-26 13:27:12 +0200

    git commit -m "Second Commit"

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,28 @@
 package com.kodilla.testing;
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania programowania.");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUserName();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+        Calculator calculator = new Calculator();
+        int result2 = calculator.add(12, 5);
+        int result3 = calculator.substract(12, 5);
+        if (result2 == 17 && result3 == 7) {
+            System.out.println("Adding and substraction work correctly.");
+        } else {
+            System.out.println("Something is wrong.");
+        }
     }
 }
+
